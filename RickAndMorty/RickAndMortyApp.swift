9926fba270cff990141b6303d3672495aa7440ac
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-08-16 18:05:26 -0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Jamerson Macedo on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
